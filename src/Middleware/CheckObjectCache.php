0bc29edfb9c1e73501c6f21c8b0a9cf6742d9b8e
Adam Crampton <adam@adamcrampton.com>
2019-11-21 07:42:32 +1100

    Moved ttl config to main class

--- src/Middleware/CheckObjectCache.php
+++ src/Middleware/CheckObjectCache.php
@@ -8,7 +8,6 @@ use AdamCrampton\ObjectCache\ObjectCache;
 
 class CheckObjectCache
 {
-    private $ttl;
     private $redis;
 
     /**
@@ -20,74 +19,6 @@ class CheckObjectCache
     {
         // Initialise Redis and set ttl values.
         $this->redis = $redis->init();
-        $this->ttl = [ 
-            // Cache store type.
-            'apc' => [
-                'default' => 86400
-            ],
-            'array' => [
-                'default' => 86400
-            ],
-            'database' => [
-                'default' => 86400
-            ],
-            'file' => [
-                'default' => 86400
-            ],
-            'memcached' => [
-                'default' => 86400
-            ],
-            'redis' => [
-                'default' => 86400
-            ],
-            // Handy values.
-            'minutes' => [
-                'one' => 60,
-                'five' => 300,
-                'ten' => 600,
-                'fifteen' => 900,
-                'twenty' => 1200,
-                'thirty' => 1800,
-                'forty' => 2400,
-                'fortyFive' => 2700,
-                'fifty' => 3000,
-                'sixty' => 3600
-            ],
-            'hours' => [
-                'one' => 3600,
-                'two' => 7200,
-                'three' => 10800,
-                'four' => 14400,
-                'five' => 18000,
-                'six' => 21600,
-                'seven' => 25200,
-                'eight' => 28800,
-                'nine' => 32400,
-                'ten' => 36000,
-                'eleven' => 39600,
-                'twelve' => 43200,
-                'twentyFour' => 86400
-            ],
-            'days' => [
-                'one' => 86400,
-                'two' => 172800,
-                'three' => 259200,
-                'four' => 345600,
-                'five' => 432000,
-                'six' => 518400,
-                'seven' => 604800,
-                'fourteen' => 1209600,
-                'twentyEight' => 2419200
-            ],
-            'weeks' => [
-                'one' => 604800,
-                'two' => 1209600,
-                'three' => 1814400,
-                'four' => 2419200,
-                'five' => 3024000,
-                'six' => 3628800
-            ]
-        ];
     }
     
     /**
